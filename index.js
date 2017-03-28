fb0dac454bb030e4bdd518c4c77a6a9c5899bfb0
VaCool <18rammzes@gmail.com>
2017-03-29 00:27:38 +0300

    Не пойму почему не работает на сервере, код рабочий...

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ var http = require('http');
 var port = 3000;
 
 var server = http.createServer(function(request, response) {
-    switch (request.url) {
-        case '/':
-            response.end("Hello");
-            break;
-    }
+	console.log(request.url);
+	var a = request.url.substring(1);
+	var b = parseInt(a);
+	var d = parseInt(a.substring(c.length+1));
+	var e = new Boolean(false);
+	if (a.substring(c.length, c.length + 1) === "-" || a.substring(c.length, c.length + 1) === "+" 
+		|| a.substring(c.length, c.length + 1) === "*" || a.substring(c.length, c.length + 1) === "/"
+		|| a.substring(c.length, c.length + 1) === "%") {
+		e = true;
+	}	
+	if (typeof(b) == "number" && typeof(b) == "number" && e == true){
+	switch(a.substring(c.length, c.length + 1)) {
+		case '-':
+			var result = b - d;
+			response.end(result);
+			break;
+		case '+':
+			response.end(b+d);
+			break;
+		case '*':
+			response.end(b*d);
+			break;
+		case '/':
+			response.end(b/d);
+			break;
+		case '%':
+			response.end(b%d);
+			break;
+	}
+	}
+	else {
+		response.end("incorrect data");
+	}
 });
 
 server.listen(port, function() {
